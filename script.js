78f5856a0eafa5d5c57901d27dfa366325194b15
Lại Quân <115360011+quanyato@users.noreply.github.com>
2022-11-16 07:30:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const gameContainer = document.querySelector('.container');
+const allMoleItems = document.querySelectorAll('.item');
+let startGame, startTime, countDown = 20, score = 0;
+
+const timeCount = document.getElementById('time-count');
+const scoreCount = document.getElementById('score-count');
+
+const theme = new Audio("theme.mp3");
+const scored = new Audio("punch.mp3");
+const missed = new Audio("woosh.mp3");
+
+gameContainer.addEventListener('click', function(e){
+    if(e.target.classList.contains('mole-clicked')){
+        score++;
+        scored.play();
+        scoreCount.innerHTML = score;
+
+        const bushElem = e.target.parentElement.previousElementSibling;
+
+        let textEl = document.createElement('span');
+        textEl.setAttribute('class', 'whack-text');
+        textEl.innerHTML = "Huhu!";
+        bushElem.appendChild(textEl);
+
+        setTimeout(() => {
+            textEl.remove();
+        }, 300);
+    } else {
+        missed.play();
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // total game time is 20 seconds
+    startTime = setInterval(() => {
+        timeCount.innerHTML = countDown;
+        countDown--;
+    }, 1000);
+
+    startGame = setInterval(() => {
+        showMole();
+    }, 600);
+    theme.play();
+});
+
+// shows mole
+function showMole(){
+    if(countDown <= 0){
+        clearInterval(startGame);
+        clearInterval(startTime);
+        timeCount.innerHTML = "0";
+    }
+    let moleToAppear = allMoleItems[getRandomValue()].querySelector('.mole');
+    moleToAppear.classList.add('mole-appear');
+    hideMole(moleToAppear);
+    
+}
+
+function getRandomValue(){
+    let rand = Math.random() * allMoleItems.length;
+    return Math.floor(rand);
+}
+
+// hide Mole
+function hideMole(moleItem){
+    setTimeout(() => {
+        moleItem.classList.remove('mole-appear');
+    }, 1000);
+}
